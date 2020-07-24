533b2f204fa441cd8f268964252ef655e3fca898
Ramona Liliana Paraschiv <ramonaliliana2015@yahoo.com>
2020-07-24 12:33:22 +0300

    first commit

---
+++ src/build/precache-manifest.04318afc11c07aefa1eff5cb57e3a6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4601e11fe52094c594b05ec6dbdcf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7035ade9cd550cfdc377",
+    "url": "/static/css/main.3854d513.chunk.css"
+  },
+  {
+    "revision": "6946c1ad9d6a70a96769",
+    "url": "/static/js/2.ce7071d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce7071d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7035ade9cd550cfdc377",
+    "url": "/static/js/main.1b0d0898.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
